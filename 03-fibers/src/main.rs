fe0b3b8bc8c77728f0db31bd0c16c9b9db27d9bc
Vladislav oleshko <vladislav.oleshko@gmail.com>
2021-07-04 23:43:44 +0300

    03-fibers updated

--- 03-fibers/src/main.rs
+++ 03-fibers/src/main.rs
@@ -1,23 +1,139 @@
+use std::rc::Rc;
+use std::cell::{Cell, RefCell, Ref};
+use std::collections::VecDeque;
+use crate::FiberState::Suspended;
+
 mod stack;
-use stack::Stack;
-fn f1(p1: usize, p2: usize) {
-    let ptr: &dyn Fn() = unsafe {std::mem::transmute((p1, p2))};
-    ptr();
-    Stack::terminate();
+
+#[derive(Copy, Clone)]
+enum FiberState {
+    Running,
+    Suspended,
+    Finished
+}
+
+struct Fiber {
+    stack: stack::Stack,
+    state: Cell<FiberState>,
+    args: (usize, usize)
+}
+
+#[derive(Default)]
+struct Executor {
+    queue: VecDeque<Rc<Fiber>>,
+    stack: stack::Stack,
+    current: Option<Rc<Fiber>>
+}
+
+fn trampoline(args: (usize, usize)) -> ! {
+    let func_ptr: &dyn Fn() = unsafe {std::mem::transmute(args)};
+    func_ptr();
+    suspend(FiberState::Finished);
+    unreachable!();
+}
+
+impl Executor {
+    fn start(&mut self) {
+        self.run();
+    }
+
+    pub fn launch<T>(&mut self, f: T)
+        where T: Fn() + 'static {
+        let args = unsafe {
+            let ptr: &dyn Fn() = &f;
+            std::mem::transmute(ptr)
+        };
+        let fiber = Rc::new(Fiber {
+            stack: stack::Stack::init(trampoline),
+            state: Cell::new(FiberState::Suspended),
+            args
+        });
+        self.queue.push_back(fiber.clone());
+    }
+
+    fn suspend(&mut self, s: FiberState) {
+        match self.current {
+            Some(ref fiber) => {
+                fiber.state.set(s);
+                stack::Stack::switch(&fiber.stack, &self.stack, (0,0));
+            },
+            _ => unreachable!()
+        }
+    }
+
+    fn run(&mut self) {
+        loop {
+            let fiber: Option<Rc<Fiber>> = self.queue.pop_front();
+            match fiber {
+                Some(fiber) => {
+                    fiber.state.set(FiberState::Running);
+                    self.current = Some(fiber.clone());
+                    self.switch_to(&fiber);
+                    match fiber.state.get() {
+                        FiberState::Finished => continue,
+                        FiberState::Suspended => self.queue.push_back(fiber),
+                        _ => unreachable!()
+                    };
+                },
+                _ => break
+            }
+        }
+    }
+
+    fn switch_to(&self, fiber: &Fiber) {
+        stack::Stack::switch(
+            &self.stack,
+            &fiber.stack,
+            fiber.args
+        );
+    }
+}
+
+static mut GE: Option<Executor> = None;
+
+fn suspend(s: FiberState) {
+    unsafe {
+        match GE {
+            Some(ref mut g) => {
+                g.suspend(s)
+            },
+            _ => unreachable!()
+        }
+    }
+}
+
+fn launch<T>(f: T)
+    where T: Fn() + 'static {
+    unsafe {
+        match &mut GE {
+            Some(ref mut g) => {
+                g.launch(f)
+            },
+            _ => unreachable!()
+        }
+    };
+}
+
+fn f2() {
+    println!("F2 part 1");
+    suspend(Suspended);
+    println!("F2 part 2");
+}
+fn f1(){
+    launch(f2);
+    println!("F1 - part 1");
+    suspend(Suspended);
+    println!("F1 - part 2");
+    suspend(Suspended);
+    println!("F1 - part 3");
 }
 
 fn main() {
-    let i = 11;
-    let mut lambda = || {
-        println!("Hello from lambda {}", i);
-    };
-    let (p1, p2): (usize, usize) = {
-        let dynptr: &dyn Fn() = &lambda;
-        unsafe {std::mem::transmute(dynptr)}
-    };
-    let stack = stack::Stack::init(f1);
-    Stack::switch_to(&stack,
-                     p1,
-                     p2);
-    println!("Works!");
+    unsafe {
+        GE = Some(Executor::default());
+        let mut g = GE.as_mut().unwrap();
+        g.launch(f1);
+        g.start();
+        GE = None;
+    }
 }
--- 03-fibers/src/stack.rs
+++ 03-fibers/src/stack.rs
@@ -1,6 +1,6 @@
-const DEFAULT_STACK_SIZE: usize = 2 * 1000; // two kilobytes
+use std::cell::{RefCell, Cell};
 
-type FuncType = fn(usize, usize);
+const DEFAULT_STACK_SIZE: usize = 10 * 1000; // ten kilobytes
 
 #[link(name = "nasm")]
 extern "C" {
@@ -8,56 +8,46 @@ extern "C" {
     fn nasm_switch(old_rsp_w: *mut usize, new_rsp_r: usize, rdi: usize, rsi: usize);
 }
 
+type FuncType = fn((usize, usize)) -> !;
+
 pub struct Stack {
-    pub bottom: usize,
-    pub top: usize,
-    align: u8,
-    allocated: bool,
+    mem: Option<Vec<u8>>,
+    bottom: usize,
+    top: Cell<usize>, // for interior mutability
 }
 
-static mut GLOBAL_STACK: Stack = Stack {bottom: 0, top: 0, align: 0, allocated: false};
+impl Default for Stack {
+    fn default() -> Self {
+        Stack {
+            mem: None,
+            bottom: 0,
+            top: Cell::new(0),
+        }
+    }
+}
+
 
 impl Stack {
     pub fn init(f: FuncType) -> Self {
-        let mut bottom = Box::into_raw(Box::new([0u8; DEFAULT_STACK_SIZE])) as usize;
-        bottom += DEFAULT_STACK_SIZE;
-        let align = (bottom % 16) as u8;
-        bottom -= align as usize;
-        let top = Self::init_stack(bottom, f);
-        Stack { bottom, top, align, allocated: true }
+        let mem = vec![0u8; DEFAULT_STACK_SIZE];
+
+        let mut bottom = (&mem[0] as *const u8) as usize + DEFAULT_STACK_SIZE;
+        bottom -= (bottom % 16);
+        let top = Cell::new(Self::init_stack(bottom, f));
+
+        Stack { mem: Some(mem), bottom, top }
     }
 
-    pub fn switch<T>(from: &mut Stack, to: &Stack, arg1: T, arg2: T)
-        where T: Into<Option<usize>> {
+    pub fn switch(from: &Stack, to: &Stack, (arg1, arg2): (usize, usize)) {
         unsafe {
-            nasm_switch(&mut from.top as *mut usize, to.top,
-                        arg1.into().unwrap_or(0),
-                        arg2.into().unwrap_or(0));
+            nasm_switch(from.top.as_ptr(), to.top.get(),
+                        arg1, arg2);
         }
-    }
-
-    pub fn switch_to<T>(to: &Stack, arg1: T, arg2: T)
-    where T: Into<Option<usize>> {
-        unsafe { Self::switch(&mut GLOBAL_STACK, to, arg1, arg2); }
-    }
-
-    pub fn terminate() {
-        let mut empty =  Stack {bottom: 0, top: 0, align: 0, allocated: false};
-        unsafe { Self::switch(&mut empty, &GLOBAL_STACK, None, None) };
     }
 
     fn init_stack(bottom: usize, f: FuncType) -> usize {
         unsafe {
             nasm_init(bottom, f)
-        }
-    }
-}
-
-impl Drop for Stack {
-    fn drop(&mut self) {
-        if self.allocated {
-            let ptr = self.bottom + self.align as usize - DEFAULT_STACK_SIZE;
-            unsafe { Box::from_raw(ptr as *mut [u8; DEFAULT_STACK_SIZE]) };
         }
     }
 }
